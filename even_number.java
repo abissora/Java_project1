48c66ce99a882721fba452cbbdb8a40deb2df1ce
alka <alka.bissora@gmail.com>
2021-02-27 18:50:43 +0000

    Commit message

---
+++ even_number.java
@@ -0,0 +1,19 @@
+package even_number;
+
+public class even_number {
+
+	public static void main(String[] args) {
+		
+		int i, j;
+		for (i=0; i<=100; i++)
+		{
+			if (i%2==0) 
+			{
+					System.out.println("Value of i: " + i);
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
